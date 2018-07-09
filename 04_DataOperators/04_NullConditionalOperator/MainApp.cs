05eb53c172665e3a29cab20577e4b51661c1825e
yybaek <baek0yun@gmail.com>
2018-07-09 17:02:41 +0900

    [C#] Chapter 04_DataOperators Commit

---
+++ 04_DataOperators/04_NullConditionalOperator/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using static System.Console;
+
+namespace NullConditionalOperator
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = null;
+            a?.Add("야구"); // a?.이 null을 반환하므로 Add() 메소드는 호출되지 않음
+            a?.Add("축구");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+
+            a = new ArrayList(); // a는 이제 더 이상 null이 아닙니다.
+            a?.Add("야구");
+            a?.Add("축구");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+        }
+    }
+}
---
+++ 04_DataOperators/04_StringConcatenate/MainApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringConcatenate
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            String result = "123" + "456";
+            Console.WriteLine(result);
+
+            result = "Hello" + " " + "World!";
+            Console.WriteLine(result);
+        }
+    }
+}
