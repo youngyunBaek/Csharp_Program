6a45abb96c0af61a1fbc118d390ccd9566192e3b
yybaek <baek0yun@gmail.com>
2018-07-16 11:36:25 +0900

    [C#] Chapter 16_Reflection Commit

---
+++ 16_Reflection/16_HistoryAttribute/MainApp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace HistoryAttribute
+{
+    [System.AttributeUsage(System.AttributeTargets.Class,
+        AllowMultiple =true)]
+    class History : System.Attribute
+    {
+        private string programmer;
+        public double version;
+        public string changes;
+
+        public History(string programmer)
+        {
+            this.programmer = programmer;
+            version = 1.0;
+            changes = "First release";
+        }
+
+        public string GetProgrammer()
+        {
+            return programmer;
+        }
+    }
+
+    [History("Sean", 
+        version = 0.1, changes = "2017-11-01 created class stub")]
+    [History("Bob", 
+        version = 0.2, changes = "2017-12-03 Added Func() method:0")]
+
+    class MyClass
+    {
+        public void Func()
+        {
+            Console.WriteLine("Func()");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(MyClass);
+            Attribute[] attribues = Attribute.GetCustomAttributes(type);
+
+            Console.WriteLine("MyClass change history...");
+
+            foreach (Attribute a in attribues)
+            {
+                History h = a as History;
+                if (h != null)
+                    Console.WriteLine("Ver:{0}, Programmer:{1}, Changes:[2}",
+                        h.version, h.GetProgrammer(), h.changes);
+            }
+        }
+    }
+}
