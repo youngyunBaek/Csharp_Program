1132b5b78b1f72b744bcd47386daf67444dfac4e
yybaek <baek0yun@gmail.com>
2018-07-18 09:48:47 +0900

    [C#] Chapter 19_Thread Commit

---
+++ C#/19_Thread/19_ParallelLoop/MainApp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelLoop
+{
+    class MainApp
+    {
+        static bool IsPrime(long number)
+        {
+            if (number < 2)
+                return false;
+
+            if (number % 2 == 0 && number != 2)
+                return false;
+
+            for (long i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            long from = Convert.ToInt64(args[0]);
+            long to = Convert.ToInt64(args[1]);
+
+            Console.WriteLine("Please press enter to start...");
+            Console.ReadLine();
+            Console.WriteLine("Started...");
+
+            DateTime startTime = DateTime.Now;
+            List<long> total = new List<long>();
+
+            Parallel.For(from, to, (long i) =>
+            {
+                if (IsPrime(i))
+                    total.Add(i);
+            });
+
+            DateTime endTIme = DateTime.Now;
+            TimeSpan ellapsed = endTIme - startTime;
+
+            Console.WriteLine("Prime number count between {0} and {1] : {2}",
+                from, to, total.Count);
+            Console.WriteLine("Ellapsed time : {0}", ellapsed);
+        }
+    }
+}
