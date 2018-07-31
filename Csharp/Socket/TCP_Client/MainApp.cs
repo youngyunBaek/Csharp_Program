a56192dc27c7eef50ae1edd754333673553ced28
yybaek <baek0yun@gmail.com>
2018-07-31 17:06:39 +0900

    Socket Program sample commit

---
+++ Csharp/Socket/TCP_Client/MainApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCP_Client
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            // IP 주소와 포트를 지정하고 TCP 연결
+            TcpClient tc = new TcpClient("127.0.0.1", 7000);
+            //TcpClient tc = new TcpClient("localhost", 7000);
+
+            string msg = "Hello world";
+            byte[] buff = Encoding.ASCII.GetBytes(msg);
+
+            // NetworkStream을 얻어옴
+            NetworkStream stream = tc.GetStream();
+
+            // 스트림 바이트 데이터 전송
+            stream.Write(buff, 0, buff.Length);
+
+            // 스트림으로부터 바이트 데이터 읽기
+            byte[] outbuf = new byte[1024];
+            int nbytes = stream.Read(outbuf, 0, outbuf.Length);
+            string output = Encoding.ASCII.GetString(outbuf, 0, nbytes);
+
+            // 스트림과 TcpClient 객체 닫기
+            stream.Close();
+            tc.Close();
+
+            Console.WriteLine($"{nbytes} bytes: {output}");
+        }
+    }
+}
---
+++ Csharp/Socket/TCP_Server/MainApp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace TCP_Server
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            // 로컬 포트 7000을 Listen
+            TcpListener listener = new TcpListener(IPAddress.Any, 7000);
+            listener.Start();
+            Console.WriteLine("Start Listen...");
+
+            byte[] buff = new byte[1024];
+
+            while(true)
+            {
+                // TcpClient Connection 요청을 받아들여
+                // 서버에서 새 TcpClient 객체를 생성하여 리턴
+                TcpClient tc = listener.AcceptTcpClient();
+                Console.WriteLine("Connect Client...");
+
+                // TcpClient 객체에서 NetworkStream을 얻어옴
+                NetworkStream stream = tc.GetStream();
+                Console.WriteLine("Get Stream...");
+
+                // 클라이언트가 연결을 끊을 때까지 데이터 수신
+                int nbytes;
+                while((nbytes = stream.Read(buff, 0, buff.Length)) > 0)
+                {
+                    // 데이터 그대로 송신
+                    stream.Write(buff, 0, nbytes);
+                }
+
+                // 스트림과 TcpClient 객체
+                stream.Close();
+                tc.Close();
+
+                // 계속 반복
+            }
+        }
+    }
+}
